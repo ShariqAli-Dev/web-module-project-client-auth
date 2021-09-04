c653a319973c04b3fba90a2f28d7edf4c9585bc8
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-09-03 20:29:12 -0400

    wrapped index.js inside of Router for re-ro-dom

--- friends/src/index.js
+++ friends/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './reducers/friends.reducer';
+import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
